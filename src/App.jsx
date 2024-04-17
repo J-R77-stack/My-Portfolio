6d6c795e33348818b2d28fffcc24bea3a96f3244
Johnny Ramsay <johnnyramsay@Johnnys-Air.lan>
2024-04-17 10:11:23 +0100

    backround inage from https://bg.ibelick.com/

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
       <div className="container mx-auto px-8">
         <Navbar />
+        <Hero />
       </div>
     </div>
   );
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,23 @@
+import { HERO_CONTENT } from "../constants";
+
 function Hero() {
-  return <div></div>;
+  return (
+    <div className="border-b border-neutral-900 pb-4 lg:mb-35">
+      <div className="flex flex-wrap">
+        <div className="w-full lg:w-1/2">
+          <div className="flex flex-col items-center lg:items-start ">
+            <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
+              Johnny Ramsay
+            </h1>
+            <span className="bg-gradient-to-r from-blue-900 via-slate-300 to bg-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
+              Junior React Developer
+            </span>
+            <p>{HERO_CONTENT}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
