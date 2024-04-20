138fd19cb4a3fd5fa9503cb81e95b344b5493ccc
Johnny Ramsay <johnnyramsay@Johnnys-Air.lan>
2024-04-20 09:57:03 +0100

    add framer motion and animate hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,15 @@
 import { HERO_CONTENT } from "../constants";
 import profilePic from "../assets/kevinRushProfile.png";
+import { motion } from "framer-motion";
+
+const container = (delay) => ({
+  hidden: { x: -100, opacity: 0 },
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: { duration: 0.5, delay: delay },
+  },
+});
 
 function Hero() {
   return (
@@ -7,20 +17,38 @@ function Hero() {
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2">
           <div className="flex flex-col items-center lg:items-start ">
-            <h1 className="pb-16 text-center sm:text-left text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
+            <motion.h1
+              initial="hidden"
+              animate="visible"
+              variants={container(0)}
+              className="pb-16 text-center sm:text-left text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
               Johnny Ramsay
-            </h1>
-            <span className="bg-gradient-to-r from-blue-900 via-slate-300 to bg-purple-500 bg-clip-text text-3xl tracking-tight text-transparent">
+            </motion.h1>
+            <motion.span
+              initial="hidden"
+              animate="visible"
+              variants={container(0.5)}
+              className="bg-gradient-to-r from-blue-900 via-slate-300 to bg-purple-500 bg-clip-text text-3xl tracking-tight text-transparent">
               Junior React Developer
-            </span>
-            <p className="my-2 max-w-xl py-6 lg:text-left text-center font-light tracking-tighter">
+            </motion.span>
+            <motion.p
+              initial="hidden"
+              animate="visible"
+              variants={container(1)}
+              className="my-2 max-w-xl py-6 lg:text-left text-center font-light tracking-tighter">
               {HERO_CONTENT}
-            </p>
+            </motion.p>
           </div>
         </div>
         <div className="w-full lg:w-1/2 lg:p-8">
           <div className="flex justify-center">
-            <img src={profilePic} alt="Johnny Ramsay" />
+            <motion.img
+              initial={{ x: 100, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 1, delay: 1.2 }}
+              src={profilePic}
+              alt="Johnny Ramsay"
+            />
           </div>
         </div>
       </div>
