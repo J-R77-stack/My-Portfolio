478469d6a96354bafee66de22973e08dcdb6f71e
Johnny Ramsay <johnnyramsay@Johnnys-Air.lan>
2024-04-20 11:27:11 +0100

    add download button for cv

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,7 @@
 import aboutImg from "../assets/about.jpg";
 import { ABOUT_TEXT } from "../constants";
 import { motion } from "framer-motion";
+import cv from "../assets/SoftwareEngineerCV.pdf";
 
 function About() {
   return (
@@ -28,6 +29,49 @@ function About() {
               {ABOUT_TEXT}
             </p>
           </div>
+          <motion.div
+            whileInView={{ opacity: 1, x: 0 }}
+            initial={{ opacity: 0, x: 100 }}
+            transition={{ duration: 1 }}
+            className="flex justify-center">
+            <button className="relative group rounded-xl overflow-hidden pl-6 h-14 flex space-x-6 items-center bg-purple-500">
+              <span className="relative uppercase text-base text-white">
+                <a href={cv} download="cv">
+                  Download CV{" "}
+                </a>
+              </span>
+              <div
+                aria-hidden="true"
+                className="w-14 bg-purple-600 transition duration-300 -translate-y-7 group-hover:translate-y-7">
+                <div className="h-14 flex">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5 m-auto fill-white"
+                    viewBox="0 0 20 20"
+                    fill="currentColor">
+                    <path
+                      fillRule="evenodd"
+                      d="M16.707 10.293a1 1 0 010 1.414l-6 6a1 1 0 01-1.414 0l-6-6a1 1 0 111.414-1.414L9 14.586V3a1 1 0 012 0v11.586l4.293-4.293a1 1 0 011.414 0z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </div>
+                <div className="h-14 flex">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5 m-auto fill-white"
+                    viewBox="0 0 20 20"
+                    fill="currentColor">
+                    <path
+                      fillRule="evenodd"
+                      d="M16.707 10.293a1 1 0 010 1.414l-6 6a1 1 0 01-1.414 0l-6-6a1 1 0 111.414-1.414L9 14.586V3a1 1 0 012 0v11.586l4.293-4.293a1 1 0 011.414 0z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </div>
+              </div>
+            </button>
+          </motion.div>
         </motion.div>
       </div>
     </div>
